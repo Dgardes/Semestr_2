617dddd238b079569bde1c017afb137ff891c1fd
Dgardes <Artem.drach.ua@gmail.com>
2026-02-23 21:38:25 +0200

    push

--- Алгоритми та структури даних/Drach_Lecture23/Drach_Lecture23/SortingAnalyzer.h
+++ Алгоритми та структури даних/Drach_Lecture23/Drach_Lecture23/SortingAnalyzer.h
@@ -88,10 +88,10 @@ class SortingAnalyzer
 				insertionSort(data);
 				break;
 			case MergeSort:
-				mergeSort(data, 0, data.size() - 1);
+				mergeSort(data, 0, (int)data.size() - 1);
 				break;
 			case QuickSort:
-				quickSort(data, 0, data.size() - 1);
+				quickSort(data);
 				break;
 			case HeapSort:
 				heapSort(data);
@@ -138,23 +138,194 @@ class SortingAnalyzer
 
 	private: void selectionSort(std::vector<int>& data)
 	{
-
+		// Проходимо по всьому масиву
+		for (int i = 0; i < data.size() - 1; i++) {
+			// Знаходимо індекс мінімального елемента
+			int minIndex = i;
+			for (int j = i + 1; j < data.size(); j++) {
+				comparisons++; //збільшуємо лічильник порівнянь
+				if (data[j] < data[minIndex]) 
+				{
+					minIndex = j;
+				}
+			}
+			// Міняємо мінімальний елемент з поточним
+			if (minIndex != i) {
+				//swap(arr[i], arr[minIndex]);
+				int temp = data[i];
+				data[i] = data[minIndex];
+				data[minIndex] = temp;
+				swaps++; //збільшуємо лічильник обмінів
+			}
+		}
 	}
 	private: void insertionSort(std::vector<int>& data)
 	{
-
+		// Починаємо з другого елемента (перший вже "відсортований")
+		for (int i = 1; i < data.size(); i++) 
+		{
+			int key = data[i]; // Елемент, який потрібно вставити
+			int j = i - 1;
+			// Зсуваємо елементи більші за key на одну позицію вправо
+			while (j >= 0 && data[j] > key) 
+			{
+				comparisons++; //збільшуємо лічильник порівнянь
+				swaps++; //збільшуємо лічильник обмінів
+				data[j + 1] = data[j];
+				j--;
+			}
+			// Вставляємо key на правильну позицію
+			data[j + 1] = key;
+		}
 	}
+
 	private: void mergeSort(std::vector<int>& data, int left, int right)
 	{
-
+		mergeSortRecursive(data, 0, (int)data.size() - 1);
 	}
-	private: void quickSort(std::vector<int>& data, int low, int high)
+
+	private: void merge(std::vector<int>& data, int left, int mid, int right) 
 	{
+		int n1 = mid - left + 1;
+		int n2 = right - mid;
 
+		//створюємо тимчасові вектори
+		std::vector<int> L(n1), R(n2);
+
+		for (int i = 0; i < n1; i++) L[i] = data[left + i];
+		for (int j = 0; j < n2; j++) R[j] = data[mid + 1 + j];
+
+		int i = 0, j = 0, k = left;
+
+		//зливаємо тимчасові вектори назад у data
+		while (i < n1 && j < n2) 
+		{
+			comparisons++; //збільшуємо лічильник порівнянь
+			if (L[i] <= R[j]) {
+				data[k] = L[i];
+				i++;
+			}
+			else {
+				data[k] = R[j];
+				j++;
+			}
+			k++;
+			swaps++; //
+		}
+
+		//додаємо залишки, якщо вони є
+		while (i < n1) 
+		{
+			data[k] = L[i];
+			i++; k++; swaps++;
+		}
+		while (j < n2) 
+		{
+			data[k] = R[j];
+			j++; k++; swaps++;
+		}
 	}
+
+	void mergeSortRecursive(std::vector<int>& data, int left, int right) 
+	{
+		if (left < right) 
+		{
+			int mid = left + (right - left) / 2;
+
+			mergeSortRecursive(data, left, mid); //сортуємо ліву частину
+			mergeSortRecursive(data, mid + 1, right); //сортуємо праву частину
+
+			merge(data, left, mid, right); //зливаємо їх
+		}
+	}
+
+	private: void quickSort(std::vector<int>& data)
+	{
+		vector<int> sortedData = recursiveQuickSort(data);
+	}
+
+	private: vector<int> recursiveQuickSort(std::vector<int>& data)
+	{
+		if (data.size() <= 1) 
+		{
+			return data;
+		}
+		int pivot = data[data.size() / 2]; //вибираємо опорний елемент
+		std::vector<int> left; //елементи менші за pivot
+		std::vector<int> right; //елементи більші за pivot
+		for (int i = 0; i < data.size(); i++) 
+		{
+			comparisons++; //збільшуємо лічильник порівнянь
+			if (data[i] < pivot) 
+			{
+				left.push_back(data[i]);
+				swaps++; //збільшуємо лічильник обмінів
+			}
+			else if (data[i] > pivot) 
+			{
+				right.push_back(data[i]);
+				swaps++; //збільшуємо лічильник обмінів
+			}
+			else 
+			{
+				swaps++; //збільшуємо лічильник обмінів для елементів, рівних pivot
+			}
+		}
+		std::vector<int> sortedLeft = recursiveQuickSort(left);
+		std::vector<int> sortedRight = recursiveQuickSort(right);
+		
+		sortedLeft.insert(sortedLeft.end(), pivot); //додаємо pivot між відсортованими частинами
+		sortedLeft.insert(sortedLeft.end(), sortedRight.begin(), sortedRight.end());
+		
+		return sortedLeft;
+	}
+	// Основна функція heap sort
 	private: void heapSort(std::vector<int>& data)
 	{
+		// Крок 1: Будуємо max heap
+		// Починаємо з останнього не-листового вузла
+		for (int i = data.size() / 2 - 1; i <= 0; i--) 
+		{ heapify(data, data.size(), i); }
 
+		// Крок 2: Витягуємо елементи з купи один за одним
+		for (int i = data.size() - 1; i > 0; i--) 
+		{
+			// Переміщуємо поточний корінь в кінець
+			int temp = data[0];
+			data[0] = data[i];
+			data[0] = temp;
+
+			// викликаємо heapify на зменшеній купі
+			heapify(data, i, 0);
+		}
+	}
+
+	void heapify(vector<int> arr, int n, int i) 
+	{
+		int largest = i; // Ініціалізуємо найбільший як корінь
+		int left = 2 * i + 1; // Лівий дочірній
+		int right = 2 * i + 2; // Правий дочірній
+		// Якщо лівий дочірній більший за корінь
+		if (left < n && arr[left] > arr[largest]) 
+		{
+			largest = left;
+		}
+		// Якщо правий дочірній більший за найбільший
+		if (right < n && arr[right] > arr[largest]) 
+		{
+			largest = right;
+		}
+		// Якщо найбільший не корінь
+
+		if (largest != i) 
+		{
+			swap(arr[i], arr[largest]);
+			int temp = arr[i];
+			arr[i] = arr[largest];
+			arr[largest] = temp;
+			// Рекурсивно heapify уражену підкупу
+			heapify(arr, n, largest);
+		}
 	}
 
 };
